7e9e86a1275bc91ba8cff1c2f235bc48a959715e
Joe <Joevandor96@gmail.com>
2022-06-16 15:54:42 -0700

    Capstone progress 6/16

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -20,7 +20,8 @@ public class VendingMachineCLI {
 	private static final String[] PURCHASE_MENU_OPTIONS = { FEED_MONEY, SELECT_PRODUCT, FINISH_TRANSACTION};
 
 	Map<String, Integer> amountAvailable = new HashMap<>();
-	int money = 0;
+	double money = 0;
+	double amountFed = 0;
 
 	private Menu menu;
 
@@ -34,88 +35,17 @@ public class VendingMachineCLI {
 			amountAvailable = newMap();
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+				productInventory();
 
-				File vendingMenu = new File("vendingmachine.csv");
-				try (Scanner input = new Scanner(vendingMenu)) {
-					while (input.hasNextLine()) {
+			}
 
-//							if ava = 0 then so out of stock
-						System.out.println(input.nextLine());
-					}
-				} catch (FileNotFoundException e) {
-					System.out.println("File Not Found.");
-				}
 
-			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+			else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
-				int amountFed = 0;
-				double amountOutput = 0;
-				setPurchaseMenuOptions();
-				int option = menu.getIn().nextInt();
+				int selection = purchaseMenu();
 
+				selection = purchaseMenu();
 
-				while (option == 1) {
-					try {
-						System.out.print("How much would you like to add?: ");
-						 money = menu.getIn().nextInt();
-
-						amountFed += money;
-						amountOutput = amountFed;
-						System.out.println("Current Money Provided: " + "$" + amountOutput);
-
-						setPurchaseMenuOptions();
-						option = menu.getIn().nextInt();
-					} catch (InputMismatchException e) {
-						System.out.println("Please enter a valid input");
-						option = 0;
-
-					}
-
-				}
-				if (option == 2) {
-					File vendingMenu = new File("vendingmachine.csv");
-					try (Scanner input = new Scanner(vendingMenu)) {
-						while (input.hasNextLine()) {
-							System.out.println(input.nextLine());
-						}
-					} catch (FileNotFoundException e) {
-						System.out.println("File Not Found.");
-					}
-
-
-					int totalAvailable = 5;
-					System.out.println("Please select an item >>> ");
-					String purchaseOption = menu.getIn().next().toUpperCase();
-
-					System.out.println("How many would you like to purchase >>> ");
-					int purchaseAmount = menu.getIn().nextInt();
-
-					if (!amountAvailable.containsKey(purchaseOption)) {
-						System.out.println("Product does not exist.");
-						setPurchaseMenuOptions();
-						option = menu.getIn().nextInt();
-					} else {
-
-						for (Map.Entry<String, Integer> newMap : amountAvailable.entrySet()) {
-
-							if (purchaseOption.equalsIgnoreCase(newMap.getKey())) {
-								if (purchaseAmount <= totalAvailable) {
-									amountAvailable.replace(purchaseOption, totalAvailable - purchaseAmount);
-									double price = itemPrice(purchaseOption); //printf
-									String type = itemOutput(purchaseOption);
-									String name = itemName(purchaseOption);
-									System.out.println(name + "|" + type + "|" + (money - (price * purchaseAmount)));
-								} else {
-									System.out.println("There are not that many available");
-
-								}
-								setPurchaseMenuOptions();
-								option = menu.getIn().nextInt();
-							}
-						}
-					}
-
-				}
 			}
 		}
 		}
@@ -133,7 +63,111 @@ public class VendingMachineCLI {
 
 	}
 
-private void setPurchaseMenuOptions() {
+	public void productInventory() {
+		int empty = 0;
+		File vendingMenu = new File("vendingmachine.csv");
+		try (Scanner input = new Scanner(vendingMenu)) {
+			while (input.hasNextLine()) {
+				String[] inputLine = input.nextLine().split("\\|");
+				if (amountAvailable.get(inputLine[0]) <= empty) {
+					System.out.println( inputLine[0] + " | " + inputLine[1] + " | " + inputLine[2] + " | " + "Remaining: SOLD OUT");
+				} else {
+					System.out.println(inputLine[0] + " | " + inputLine[1] + " | " + inputLine[2] + " | " + "Remaining: " + amountAvailable.get(inputLine[0]));
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found.");
+		}
+	}
+
+	public int purchaseMenu(){
+		setPurchaseMenuOptions();
+		int option = menu.getIn().nextInt();
+		if(option > 3){
+			System.out.println("Please Select a valid option.\n");
+			option = purchaseMenu();
+		}
+		while(option == 1){
+			money = feedMoney(money);
+			option = purchaseMenu();
+		}
+		while(option == 2){
+			productInventory();
+			System.out.println("Please select an item >>> ");
+			String purchaseOption = menu.getIn().next().toUpperCase();
+			System.out.println("How many would you like to purchase >>> ");
+			int purchaseAmount = menu.getIn().nextInt();
+			selectProduct(purchaseOption,purchaseAmount);
+			option = purchaseMenu();
+		}
+
+		return option;
+	}
+
+
+	public double feedMoney(double insertedMoney){
+		int amountFed = 0;
+		double amountOutput = 0;
+		try {
+			System.out.print("How much would you like to add?: ");
+			money = menu.getIn().nextInt();
+
+			amountFed += money;
+			amountOutput = amountFed + insertedMoney;
+			System.out.println("Current Money Provided: " + "$" + amountOutput);
+
+
+
+		} catch (InputMismatchException e) {
+			System.out.println("Please enter a valid input");
+		}
+			return amountOutput;
+	}
+
+	public void selectProduct(String option, int amount){
+		if (!amountAvailable.containsKey(option)) {
+						System.out.println("Product does not exist.");
+						purchaseMenu();
+					} else {
+
+						for (Map.Entry<String, Integer> newMap : amountAvailable.entrySet()) {
+
+							if (option.equalsIgnoreCase(newMap.getKey())) {
+								if (amount <= newMap().get(option)) {
+									amountAvailable.replace(option, newMap().get(option) - amount);
+									double price = itemPrice(option); //printf
+									String type = itemOutput(option);
+									String name = itemName(option);
+									money -= price*amount;
+									System.out.println(name + "|" + type + "|" + (money));
+								} else {
+									System.out.println("There are not that many available");
+
+								}
+							}
+						}
+					}
+
+				}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public void setPurchaseMenuOptions() {
 	int i = 1;
 	for (Object purchaseMenuOption : VendingMachineCLI.PURCHASE_MENU_OPTIONS) {
 		System.out.println(i + ") " + purchaseMenuOption);
@@ -141,6 +175,7 @@ private void setPurchaseMenuOptions() {
 	}
 	System.out.print("\nPlease choose an option >>> ");
 }
+
 
 public Map<String, Integer> newMap(){
 	int available = 5;
@@ -152,11 +187,12 @@ public Map<String, Integer> newMap(){
 		} catch (FileNotFoundException e) {
 			System.out.println("File Not Found.");
 		}
-
 return amountAvailable;
-
-
 }
+
+
+
+
 
 public Double itemPrice(String index){
 	File vendingMenu = new File("vendingmachine.csv");
@@ -174,6 +210,10 @@ public Double itemPrice(String index){
 	}
 		return price;
 }
+
+
+
+
 
 public String itemOutput(String index){
 	File vendingMenu = new File("vendingmachine.csv");
@@ -201,6 +241,9 @@ public String itemOutput(String index){
 	return output;
 }
 
+
+
+
 public String itemName(String index){
 	File vendingMenu = new File("vendingmachine.csv");
 	String name = "";
@@ -217,4 +260,29 @@ public String itemName(String index){
 	}
 	return name;
 }
+
+//	public String itemCost(String index){
+//		File vendingMenu = new File("vendingmachine.csv");
+//		String name = "";
+//		try (Scanner input = new Scanner(vendingMenu)) {
+//			while (input.hasNextLine()) {
+//				String thisInput = input.nextLine();
+//				if(thisInput.contains(index)) {
+//					String[] purchasedItem = thisInput.split("\\|");
+//					name = purchasedItem[1];
+//				}
+//			}
+//		} catch (FileNotFoundException e) {
+//			System.out.println("File Not Found.");
+//		}
+//		return name;
+//	}
+
+
+
+
+
+
+
+
 }
